f89de2dbbde21444b258db3567b78f15bbd75c05
shahina597 <128354440+shahina597@users.noreply.github.com>
2023-06-13 22:53:42 -0700

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,12 @@
+function myCheck(){
+	var pass1=document.getElementById('password');
+	var pass2=document.getElementById('c-password');
+	
+	if(pass1.type=="password" || pass2.type=="password"){
+		pass1.type="text";
+		pass2.type="text";
+	}else{
+		pass1.type="password";
+		pass2.type="password";
+	}
+}
